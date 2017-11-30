06536f2a87797a9a9fff98f0035773b1dd919867
Ubuntu <abecerra@ual.es>
2017-11-30 10:12:21 +0000

    Controladores

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
+/* Location Pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo); 
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Other Pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
