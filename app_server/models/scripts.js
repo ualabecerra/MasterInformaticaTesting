6ce7032d03979504c8788986d5ed2e5a4e18de6a
Ubuntu <abecerra@ual.es>
2017-12-03 18:34:28 +0000

    Heroku database

---
+++ app_server/models/scripts.js
@@ -0,0 +1,254 @@
+db.locations.save({name: 'Starcups',
+	        address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: [-0.9690884,51.455041],
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }]
+        })
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Simon Holmes',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Antonio Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Daniel Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+
+db.locations.save({name: 'Entrevinos',
+	        address: 'Callzada de Castro 45',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: [-2.4499777,36.8388359],
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [
+            {	author: 'Ana Becerra',
+				id: ObjectId(),
+				rating: 5,
+				timestamp: new Date("Nov 30, 2017"),
+				reviewText: "What a great place. I can't say enough good things about it."
+				}, 
+				{
+				author: 'Manolo Torres',
+				id: ObjectId(),
+				rating: 5,
+				timestamp: new Date("Dec 1, 2017"),
+				reviewText: "What a great place. I can't say enough good things about it."
+				}, {
+				author: 'Daniel Becerra',
+				id: ObjectId(),
+				rating: 5,
+				timestamp: new Date("Oct 15, 2017"),
+				reviewText: "What a great place. I can't say enough good things about it."}
+				]
+        })
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Ana Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Antonio Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Daniel Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+db.locations.save({name: 'Starcups',
+	        address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: [-0.9690884,51.455041],
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }]
+        })
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Simon Holmes',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Antonio Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+db.locations.update({
+name: 'Starcups'
+}, {
+$push: {
+reviews: {
+author: 'Daniel Becerra',
+id: ObjectId(),
+rating: 5,
+timestamp: new Date("Jul 16, 2013"),
+reviewText: "What a great place. I can't say enough good things about it."
+}
+}
+})
+
+
+db.locations.save({name: 'Bar Lamarca',
+	        address: 'Calle Gregorio Maranon',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: [-2.4570093,36.8380492],
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [
+            {	author: 'Ana Becerra',
+				id: ObjectId(),
+				rating: 5,
+				timestamp: new Date("Nov 30, 2017"),
+				reviewText: "What a great place. I can't say enough good things about it."
+				}, 
+				{
+				author: 'Manolo Torres',
+				id: ObjectId(),
+				rating: 5,
+				timestamp: new Date("Dec 1, 2017"),
+				reviewText: "What a great place. I can't say enough good things about it."
+				}, {
+				author: 'Daniel Becerra',
+				id: ObjectId(),
+				rating: 5,
+				timestamp: new Date("Oct 15, 2017"),
+				reviewText: "What a great place. I can't say enough good things about it."}
+				]
+        })
